1ca2697704c16adb5053f15f1842beb53a39025e
Samuel Chu <samueljchu0126@gmail.com>
2024-06-01 14:58:18 -0400

    left sidebar in progress, created constant files to export arrays of pertinent information

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,17 +1,26 @@
+import { sidebarLinks } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 const LeftSidebar = () => {
-  return (
-    <section className='left-sidebar'>
-        <nav className='flex flex-col gap-6'>
-            <Link href='/'>
-                <Image src=''/>
-            </Link>
-        </nav>
-    </section>
-  )
+    return (
+        <section className='left-sidebar'>
+            <nav className='flex flex-col gap-6'>
+                <Link href='/' className='flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center'>
+                    <Image src='/icons/logo.svg' alt='logo' width={23} height={27} />
+                    <h1 className="text-24 font-extrabold text-white-1 max-lg:hidden">Podcastr</h1>
+                </Link>
+
+                {sidebarLinks.map(({route, label}) => {
+
+                    return <Link href={route}>
+                        {label}
+                    </Link>
+                })}
+            </nav>
+        </section>
+    )
 }
 
-export default LeftSidebar
+export default LeftSidebar 
